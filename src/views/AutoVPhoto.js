85660581796724050f66a0079abcd877b305dbba
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-03 21:03:12 -0500

    added basic info to auto v photos

--- src/views/AutoVPhoto.js
+++ src/views/AutoVPhoto.js
@@ -1,5 +1,15 @@
 const AutoVPhoto = (props) => {
-  return <div>AutoVPhoto</div>;
+  return 
+  <div>
+   <h1>AutoVPhoto</h1>
+   <div>
+     <p>So what is the difference between AutoFlowers and PhotoPeriod plants?</p>
+     <p>We are happy you asked, the main difference is the amount of light required for each of thse plants to grow</p>
+     <p>AutoFlowers will go through all the stages of their life-cycle regardless of hours of light, while PhotoPeriods
+       require certain amounts of light to move beyond each cycle.
+     </p>
+   </div>
+  </div>;
 };
 
 export default AutoVPhoto;
