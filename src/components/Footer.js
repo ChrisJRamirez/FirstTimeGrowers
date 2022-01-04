fd5faa03fc96f7136e919b7a402e6b5918c164f0
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-03 21:52:09 -0500

    built basic footer with socials/links to pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,58 @@
+
 const Footer = (props) => {
+
+  const socials = [
+    {
+      name: 'Github',
+      // what is the best way to import social logos and use them on our page, sizing has to all be the same ***
+      logo: '/Users/chrisjramirez/Documents/FirstTimeGrowers/FirstTimeGrowers/src/assets/GitHub-Mark-64px.png',
+      to: 'https://github.com/ChrisJRamirez/FirstTimeGrowers',
+    },
+    {
+      name: 'Twitter',
+      logo: '',
+      to: 'https://twitter.com/FTGrowers',
+    },
+    {
+      name: 'Reddit',
+      logo: '',
+      to: 'https://www.reddit.com/user/TheFirstTimeGrowers',
+    },
+    {
+      name: 'Instagram',
+      logo: '',
+      to: 'https://www.instagram.com/thefirsttimegrowers/',
+    },
+  ];
+
   return (
     <div>
-      me footer
-      <div>Team Karpe Shariq Chris</div>
+      <div>
+        <footer>
+          <div style={{
+            backgroundColor: '#97aaa7',
+          }}
+            className='flex items-center justify-center w-full h-full m-auto rounded-lg shadow-lg lg:w-1/2'>
+            <div>
+              <ul>
+                {socials.map((item) => {
+                  return (
+                    <li className='p-3 hover:bg-brown-200 hover:text-black'>
+                      <a className='font-bold' href={item.to}>
+                        {item.name}
+                      </a>
+                      {item.subtext && <p>{item.subtext}</p>}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+
+          </div>
+        </footer>
+
+
+      </div>
     </div>
   );
 };
--- src/views/AutoVPhoto.js
+++ src/views/AutoVPhoto.js
@@ -1,15 +1,16 @@
 const AutoVPhoto = (props) => {
-  return 
-  <div>
-   <h1>AutoVPhoto</h1>
-   <div>
-     <p>So what is the difference between AutoFlowers and PhotoPeriod plants?</p>
-     <p>We are happy you asked, the main difference is the amount of light required for each of thse plants to grow</p>
-     <p>AutoFlowers will go through all the stages of their life-cycle regardless of hours of light, while PhotoPeriods
-       require certain amounts of light to move beyond each cycle.
-     </p>
-   </div>
-  </div>;
-};
+  return (
+    <div>
+      <h1>AutoVPhoto</h1>
+      <div>
+        <p>So what is the difference between AutoFlowers and PhotoPeriod plants?</p>
+        <p>We are happy you asked, the main difference is the amount of light required for each of thse plants to grow</p>
+        <p>AutoFlowers will go through all the stages of their life-cycle regardless of hours of light, while PhotoPeriods
+          require certain amounts of light to move beyond each cycle.
+        </p>
+      </div>
+    </div>
+  )
+}
 
 export default AutoVPhoto;
