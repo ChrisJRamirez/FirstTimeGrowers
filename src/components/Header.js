e09e8c7379f001f797d62bde381cbab78fd90132
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-04 13:29:55 -0500

    style basic header with similar theme to rest of page, still needs work

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,30 @@
 const Header = (props) => {
   return (
     <div className='flex flex-col py-2'>
-      <body className="font-sans leading-normal tracking-normal bg-gray-400">
-
-          <nav className="fixed top-0 z-10 flex flex-wrap items-center justify-between w-full p-6 bg-gray-800">
+      <header>
+          <nav style={{
+            backgroundColor: '#97aaa7',
+          }}
+          className="fixed top-0 z-10 flex flex-wrap justify-between w-full p-6 ">
             <div className="flex items-center flex-shrink-0 mr-6 text-white">
-          <a className="text-white no-underline hover:text-white hover:no-underline" href="/">
-            <span className="pl-2 text-2xl"><i className="em em-grinning"></i>First Time Growers</span>
+          <a className="text-white no-underline hover:text-white hover:no-underline" href="">
+            <h2 className="pl-2 text-3xl">First Time Growers</h2>
           </a>
         </div>
 
-        <div className="block lg:hidden">
-          <button id="nav-toggle" className="flex items-center px-3 py-2 text-gray-500 border border-gray-600 rounded hover:text-white hover:border-white">
-            <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-          </button>
-        </div>
-
         <div className="flex-grow hidden pt-6 lg:flex lg:items-center lg:w-auto lg:block lg:pt-1" id="nav-content">
-          <ul className="items-center justify-end flex-1 list-reset lg:flex">
+          <ul className="items-center flex-1 list-reset lg:flex">
             <li className="">
-              <a className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline" href="#">Active</a>
+              <a className="inline-block px-4 py-2 font-bold text-black no-underline hover:text-black hover:text-underline hover:bg-brown-200" href="faq">FAQ</a>
             </li>
             <li className="">
-              <a className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline" href="#">Mission</a>
+              <a className="inline-block px-4 py-2 font-bold text-black no-underline hover:text-black hover:text-underline hover:bg-brown-200" href="">Mission</a>
             </li>
             <li className="">
-              <a className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline" href="#">Contact</a>
+              <a className="inline-block px-4 py-2 font-bold text-black no-underline hover:text-black hover:text-underline hover:bg-brown-200" href="">Contact</a>
             </li>
             <li className="">
-              <a className="inline-block px-4 py-2 text-gray-600 no-underline hover:text-gray-200 hover:text-underline" href="#">Team</a>
+              <a className="inline-block px-4 py-2 font-bold text-black no-underline hover:text-black hover:text-underline hover:bg-brown-200" href="">Team</a>
             </li>
           </ul>
         </div>
@@ -39,7 +35,7 @@ const Header = (props) => {
       <div className="container mx-auto mt-24 bg-white shadow-lg md:mt-18">
 
       </div>
-  </body>
+  </header>
 
   </div>
   );
