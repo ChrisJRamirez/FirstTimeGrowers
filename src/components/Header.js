bc442d6d994c9e8d593b43157b3ee219131f6f10
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-05 23:35:02 -0500

    removed FTG route to /, lets see if it compiles now

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,11 +34,7 @@ const Header = (props) => {
         className='fixed z-50 w-full rounded-lg'
       >
         <div className='flex justify-between mx-10'>
-          <Link to="/">
           <h2 className='my-auto text-center' >First Time Growers</h2>
-          </Link>
-          
-
           <nav>
             <ol className='flex'>
               {headNav.map((item) => {
