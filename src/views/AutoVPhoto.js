92ee25a214b50d8224f412b004cf6a67cbf4a5c6
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-05 21:16:41 -0500

    adding info to AuthoVPhoto

--- src/views/AutoVPhoto.js
+++ src/views/AutoVPhoto.js
@@ -1,13 +1,33 @@
 const AutoVPhoto = (props) => {
+
+  const headNav = [
+    {
+      info: 'Simple answer: Autoflower. Autoflowers don’t require seasonal signals to initiate flowering.Cannabis Ruderalis, a strain native to Russia, has the ability to flower despite consistent light and weather. This subspecies was crossed with Indica and Sativa to form hybrids that “automatically” flower after a set period of time. Hence the name, Autoflower.',
+    },
+    {
+      info: 'Photoperiods require you to understand the basics of growing a bit more before you invest in the effort. Photoperiods will stay in a Vegetative state as long as the lights are on for 18+ hours a day. Once you want them to start Flowering you switch the lights to a cycle of 12hours on, 12hours off.',
+    },
+    {
+      info: 'When Autoflowers came on the the scene they were useful, but the yields were smaller because the plants spent less time in the Vegetative cycle (which builds the plants stems, leaves and overall size) they would produce lower yields due to there being less places for buds to grow (or to grow large). Nowadays that problem is almost non-existent as modern Autoflowers produce similar yields to their Photoperiod cousins. Obviously if you are growing in a very large medium or outdoors then Photoperiods will always produce more. ',
+    },
+    {
+      info: 'Most Autoflowers are sold as “feminized” but make sure the seeds you are buying advertise this as not all Auto’s are feminized. Feminized means that the seed will guarantee a female. You want this as male plants produce seeds and have smaller yields than female plants.',
+    },
+    {
+      info: 'If you are looking to have a plant that goes from seed to harvest within 10-12 weeks, minimalize any complication with light schedules, has very good yields, and is resilient to pests and fungus: go Auto.',
+    },
+  ]
+ 
+
   return (
     <div>
-      <h1>AutoVPhoto</h1>
-      <div>
-        <p>So what is the difference between AutoFlowers and PhotoPeriod plants?</p>
-        <p>We are happy you asked, the main difference is the amount of light required for each of thse plants to grow</p>
-        <p>AutoFlowers will go through all the stages of their life-cycle regardless of hours of light, while PhotoPeriods
-          require certain amounts of light to move beyond each cycle.
-        </p>
+      <h1>Autoflower vs. Photoperiod</h1>
+      <div style={{
+            backgroundColor: '#97aaa7',
+          }}
+          className='w-full h-full py-10 m-auto rounded-lg shadow-lg'
+      >
+      
       </div>
     </div>
   )
